ed2685a4cf98dc9b1e0cd52bf74cece3b41de929
NoumanShah042 <noumanrehman042@gmail.com>
2022-01-13 16:53:40 +0500

    Database CDUD operations

---
+++ app/src/main/java/com/example/sqliteapplication/DBHelper.java
@@ -0,0 +1,87 @@
+package com.example.sqliteapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        // it create database with name Userdata.db
+        super(context, "Userdata.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table Userdetails(name TEXT primary key, contact TEXT, dob TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop Table if exists Userdetails");
+    }
+
+    public Boolean insertUserData(String name, String contact, String dob) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("contact", contact);
+        contentValues.put("dob", dob);
+        long result = DB.insert("Userdetails", null, contentValues);
+        //  DB.insert return the row ID of the newly inserted row, or -1 if an error occurred
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    public Boolean updateUserData(String name, String contact, String dob) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        // we only want to update contact and dob  for the given name
+        contentValues.put("contact", contact);
+        contentValues.put("dob", dob);
+        //  first we will get all the records with the given name
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.update("Userdetails", contentValues, "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    public Boolean deleteData(String name) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.delete("Userdetails", "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    public Cursor getData() {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Userdetails", null);
+        return cursor;
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Android SQLite Database
+
+
+### Developed By [Syed Nouman](https://github.com/NoumanShah042)
+ 
+                
+
+## Screenshots
+
+* Main Page
+<br><br>
+<img src="screenshots/a.png" width="300">
+
+* Showing data 
+<br><br>
+<img src="screenshots/b.png" width="300">
